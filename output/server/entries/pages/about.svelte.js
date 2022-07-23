449d23de74efdf7acf6f7de1c0f8c9e037190381
Tobbelowski <tobelowski@gmail.com>
2022-07-23 21:42:15 +0200

    Updates

---
+++ output/server/entries/pages/about.svelte.js
@@ -0,0 +1,8 @@
+import { c as create_ssr_component } from "../../_app/immutable/chunks/index-3e01a4ca.js";
+const About = create_ssr_component(($$result, $$props, $$bindings, slots) => {
+  return `<h2>About page</h2>
+<div>Ciao!</div>`;
+});
+export {
+  About as default
+};
---
+++ output/server/nodes/2.js
@@ -0,0 +1,7 @@
+import * as module from '../entries/pages/about.svelte.js';
+
+export { module };
+export const index = 2;
+export const file = '_app/immutable/pages/about.svelte-0c68a8e8.js';
+export const imports = ["_app/immutable/pages/about.svelte-0c68a8e8.js","_app/immutable/chunks/index-6756eaca.js"];
+export const stylesheets = [];
